bef6692853769393639c4e6a6e7c02fd717d42b1
hi6nika <georgiew.m57@gmail.com>
2023-12-09 04:55:53 +0200

    Update boughtOrNot.js

--- ReactExam-Nikolay/src/utils/boughtOrNot.js
+++ ReactExam-Nikolay/src/utils/boughtOrNot.js
@@ -1,21 +1,10 @@
-  
- 
+export default function boughtOrNot(buyers, _id) {
+  const arr = [];
 
-export default  function  boughtOrNot(buyers, _id) {
-
- 
-
-    // for ([key, values] of Object.entries(buyers)) {
-  
-    // }
-  
-    // if (!alreadyBought.includes(buyerDetails._id)) {
-    //   car.buyers.push(buyerDetails);
- 
-  
-    //   }
-
-   
-    return  true;
+  for (const [keys, values] of Object.entries(buyers)) {
+    arr.push(values._id);
   }
-  
+ 
+
+  return !arr.includes(_id);
+}
